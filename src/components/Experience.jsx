c8866514a26fe3550279ecacb16caa9f1186685b
JoakinGC <joakingcontreras2@gmail.com>
2024-06-16 18:50:31 +0200

    add

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,13 @@
 import React, { useRef } from "react";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { FaGithub } from "react-icons/fa";
+import "react-vertical-timeline-component/style.min.css";
+import "./portfolio.scss";
 import { SectionWrapper } from "../hoc";
+
 import { useTranslation } from "react-i18next";
 import { experience } from "../constants";
+
 
 const ExperienceCard = ({ item }) => {
   const { t } = useTranslation();
@@ -16,19 +20,19 @@ const ExperienceCard = ({ item }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section className="my-10">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col lg:flex-row items-center lg:items-start">
-          <div className="lg:w-1/2 w-full mb-4 lg:mb-0" ref={ref}>
-            <img src={item.img} alt="" className="w-full h-auto rounded-lg" />
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt=""  />
           </div>
-          <motion.div className="lg:w-1/2 w-full lg:pl-10" style={{ y }}>
-            <h2 className="text-2xl font-bold mb-2">{t(item.title)}</h2>
-            <p className="mb-4">{t(item.desc)}</p>
-            <div className="flex space-x-4">
-              <a href={`${item.link}`} target="_blank" className="btn btn-primary">See Demo</a>
-              <a href={`${item.linkGitHub}`} target="_blank" className="btn btn-secondary flex items-center">
-                <FaGithub className="mr-2" /> View Code
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{t(item.title)}</h2>
+            <p>{t(item.desc)}</p>
+            <div className="buttons">
+              <a href={`${item.link}` } target="_blank" className="demoButton">See Demo</a>
+              <a href={`${item.linkGitHub}`} target="_blank" className="codeButton">
+                <FaGithub /> View Code
               </a>
             </div>
           </motion.div>
@@ -54,8 +58,8 @@ const Experience = () => {
 
   return (
     <div className="portfolio" ref={ref}>
-      <div className="progress mb-10">
-        <h1 className="text-3xl font-bold">{t("Featured Works")}</h1>
+      <div className="progress">
+        <h1>{t("Featured Works")}</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {experience.map((item) => (
