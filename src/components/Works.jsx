3171562070e9bb67df9801630bc41b12e17a6ee4
JoakinGC <joakingcontreras2@gmail.com>
2024-06-17 14:11:27 +0200

    add

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -21,7 +21,7 @@ const ProjectCard = ({
   const { t } = useTranslation();
 
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)} initial="hidden" animate="show">
       <Tilt
         options={{
           max: 45,
@@ -76,14 +76,14 @@ const Works = () => {
 
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant()} initial="hidden" animate="show">
         <p className={`${styles.sectionSubText} `}>{t('My work')}</p>
         <h2 className={`${styles.sectionHeadText}`}>{t('Projects.')}</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          variants={fadeIn("", "", 0.1, 1)} initial="hidden" animate="show"
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           {t('Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.')}
